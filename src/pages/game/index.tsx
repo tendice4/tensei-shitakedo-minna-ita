d06b716c5d3775445101235cbe6475bddc96d300
gen gen <bonmown@gmail.com>
2021-08-06 00:39:42 +0900

    a

---
+++ src/pages/game/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import s from "/src/styles/pages/game/index.module.css";
+type Props = {};
+
+const App: React.FC = (props: Props) => {
+  return (
+    <div className={s.container}>
+      <div className={s.content}>転生したけどみんないたｗ</div>
+    </div>
+  );
+};
+
+export default App;

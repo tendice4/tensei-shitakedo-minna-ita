69a445631f281244736fc2ae965259d27b427556
tendice4 <tendice4@icloud.com>
2021-08-06 00:20:16 +0900

    index作成

---
+++ src/pages/hoge.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Hoge: React.FC = (props: Props) => {
+  return <div>Hoge</div>;
+};
+
+export default Hoge;
---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const App: React.FC = (props: Props) => {
+  return <div>転生したけどみんないたｗ</div>;
+};
+
+export default App;

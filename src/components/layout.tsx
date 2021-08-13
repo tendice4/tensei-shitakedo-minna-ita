2618cd56ad27697131a30f13db888d1dfe3e3da0
gen gen <bonmown@gmail.com>
2021-08-14 08:37:45 +0900

    a

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
 import s from "/src/styles/components/elements/layout.module.css";
 
+const MAX_WIDTH = 578;
+
 const Layout: React.FC = ({ children }) => {
+  const [innerWidth, setInnerWidth]: [
+    number,
+    React.Dispatch<SetStateAction<number>>
+  ] = useState(578);
+  useEffect(() => {
+    window.onresize = () => {
+      setInnerWidth(window.innerWidth);
+    };
+  }, []);
   return (
     <div className={s.container}>
-      <div className={s.content}>{children}</div>
+      <div
+        className={s.content}
+        style={
+          innerWidth < MAX_WIDTH
+            ? {
+                zoom: `${(innerWidth / MAX_WIDTH) * 100}%`,
+              }
+            : {}
+        }
+      >
+        {children}
+      </div>
     </div>
   );
 };
